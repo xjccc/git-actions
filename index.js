2b0a11950c551e0a08572c8bef81721c55e9a104
xjccc <jing.wang-fe@360che.com>
2022-02-08 09:19:01 +0800

    xj_update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// github api
-// octokit
-
+// https://docs.github.com/cn/actions/learn-github-actions/events-that-trigger-workflows
+// https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
+const { Octokit } = require('octokit')
 // TODO: auth
-const octokit = new Octokit({ auth: 'personal-access-token123' })
+// settings => developsettings => personal code
+const octokit = new Octokit({
+  auth: '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'
+})
 
 octokit.rest.issues.create({
   owner: 'xjccc',
   repo: 'git-actions',
-  title: 'Hello world from '
+  title: 'second issues',
+  body: '哈哈哈'
 })
+
+// 自动执行

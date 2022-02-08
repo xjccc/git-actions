276309d7a83c33678901cb8af2296fb4e047c246
xjccc <jing.wang-fe@360che.com>
2022-02-08 09:35:26 +0800

    xj_update

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // https://docs.github.com/cn/actions/learn-github-actions/events-that-trigger-workflows
 // https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
 const { Octokit } = require('octokit')
+const core = require('@actions/core')
 // TODO: auth
 // settings => developsettings => personal code
+// 明文token使用git actions 会失效
+// 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
+// profiles -> secrets -> actions
+
+// 通过core获取token
+const token = core.getInput('token')
 const octokit = new Octokit({
-  auth: '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'
+  auth: token
 })
 
 octokit.rest.issues.create({
